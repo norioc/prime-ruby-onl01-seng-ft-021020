8404a935aa4f0176a851645b67a271c24bb7675f
norio c <norionorioc@gmail.com>
2020-02-14 04:30:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |trial|
+      number % trial == 0
+    end
+  else
+    FALSE
+  end
+end
