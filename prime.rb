02488d474f01c1b36dc34c1408e809e24ec891e8
norio c <norionorioc@gmail.com>
2020-02-14 04:23:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,12 @@
 def prime?(number)
-  
+  if value <= 1 || value == 0 || value == 1
+    return false 
+  elsif 
+    (2..value - 1).each do |i|
+      if value % 1 == 0
+      return false
+    end
+  end
+end
+true
+end
